539d79df110d1dbf9c3306490b60bb7f681248af
Kiinyo <iiyoheki@gmail.com>
2021-02-09 21:59:42 -0700

    a simple buffer reader so I can write a parser

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,4 +1,20 @@
 extern crate kiiterm;
+use std::io::{Write};
 fn main() {
-    let screen = kiiterm::screen::init(100, 25);
+    let mut screen = kiiterm::screen::init(100, 5);
+
+    loop {
+        let mut buffer = Vec::new();
+        use std::io::*;
+        screen.inputs.read_to_end(&mut buffer).unwrap();
+        writeln!(screen.context,
+        "{}{}Buffer: {:?}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        buffer)
+       .unwrap();
+
+        screen.context.flush().unwrap();
+        std::thread::sleep(std::time::Duration::from_millis(2000));
+    }
 }
