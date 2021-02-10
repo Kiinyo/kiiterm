ad7fc05228032c7930f71345e1eaeeff6ce8b0a8
Kiinyo <iiyoheki@gmail.com>
2021-02-10 14:04:46 -0700

    added input enums

--- src/screen.rs
+++ src/screen.rs
@@ -23,3 +23,110 @@ pub fn init (width: u16, height: u16) -> Screen {
     // Finally return the screen to be used
     Screen {width, height, context, inputs}
 }
+
+pub mod input {
+    pub enum Key {
+        // a..Z
+        Char(char),
+        Alt_Char(char),
+        // A..Z
+        // The terminal doe not recognize cae with Ctrl
+        Ctrl_Char(char),
+
+        // Any combination not listed is due to a limitation
+        // of the terminal (...usually)
+
+        // Backspace
+
+        Backspace,
+
+        Alt_Backspace,
+
+        // Delete
+
+        Delete,
+
+        Shift_Delete,
+        Alt_Delete,
+        Ctrl_Delete,
+
+        // Arrow Keys
+
+        Up,
+        Down,
+        Right,
+        Left,
+
+        Shift_Up,
+        Shift_Down,
+        Shift_Right,
+        Shift_Left,
+
+        Alt_Up,
+        Alt_Down,
+        Alt_Right,
+        Alt_Left,
+
+        Ctrl_Up,
+        Ctrl_Down,
+        Ctrl_Right,
+        Ctrl_Left,
+
+        // Escape - No other variations work it also
+        // shares 27 with escape codes so if escape
+        // is pressed with another key in the same 
+        // frame it will not register so be careful!
+        Escape,
+
+        // Insert
+
+        Insert,
+
+        // Home
+
+        Home,
+
+        // Page
+        
+        Page_Up,
+        Page_Down,
+
+        // End
+
+        End,
+
+        // Null, junk/invalid/no inputs
+
+        Null
+    }
+    pub enum Modifier {
+        Alt,
+        Shift,
+        Ctrl,
+
+        None
+    }
+    /// Follows Enum(x, y, Modifier)
+    pub enum Mouse {
+        // Left Mouse Button
+        LMB_Press {x: u16, y: u16, modifier: Modifier},
+        LMB_Release {x: u16, y: u16, modifier: Modifier},
+        LMB_Move {x: u16, y: u16, modifier: Modifier},
+
+        // Middle Mouse Button
+        MMB_Press {x: u16, y: u16, modifier: Modifier},
+        MMB_Release {x: u16, y: u16, modifier: Modifier},
+        MMB_Move {x: u16, y: u16, modifier: Modifier},
+
+        // Right Mouse Button
+        RMB_Press {x: u16, y: u16, modifier: Modifier},
+        RMB_Release {x: u16, y: u16, modifier: Modifier},
+        RMB_Move {x: u16, y: u16, modifier: Modifier},
+
+        // Scrolling
+        Scroll_Up {x: u16, y: u16, modifier: Modifier},
+        Scroll_Down {x: u16, y: u16, modifier: Modifier}
+    }
+
+    
+}
