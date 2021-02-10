f20f722af88416e6fd29a1cd6d6e7bf9aa67300e
Kiinyo <iiyoheki@gmail.com>
2021-02-09 21:59:19 -0700

    added non-blocking input reading

--- src/screen.rs
+++ src/screen.rs
@@ -1,10 +1,10 @@
 use crate::terminal;
 /// The structure that holds everything needed to handle inputs and draw to thet terminal
 pub struct Screen {
-    width: u16,
-    height: u16,
-    context: termion::input::MouseTerminal<termion::raw::RawTerminal<std::io::Stdout>>,
-    inputs: std::io::Stdin
+    pub width: u16,
+    pub height: u16,
+    pub context: termion::input::MouseTerminal<termion::raw::RawTerminal<std::io::Stdout>>,
+    pub inputs: termion::AsyncReader
 }
 /// Create the Screen to be used by nearly everything
 pub fn init (width: u16, height: u16) -> Screen {
@@ -18,7 +18,7 @@ pub fn init (width: u16, height: u16) -> Screen {
     // Create the context we'll be drawing things to
     let context= termion::input::MouseTerminal::from(std::io::stdout().into_raw_mode().unwrap());
     // Get a way to funnel player inputs
-    let inputs = std::io::stdin();
+    let inputs = termion::async_stdin();
 
     // Finally return the screen to be used
     Screen {width, height, context, inputs}
