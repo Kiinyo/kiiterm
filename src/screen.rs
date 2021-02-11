37482448a3f46973b5b8b929537e69688ce3d5dc
Kiinyo <iiyoheki@gmail.com>
2021-02-10 19:09:30 -0700

    added parse_char, not my finest moment

--- src/screen.rs
+++ src/screen.rs
@@ -32,6 +32,7 @@ pub mod input {
         // A..Z
         // The terminal doe not recognize cae with Ctrl
         Ctrl_Char(char),
+        Ctrl_Alt_Char(char),
 
         // Any combination not listed is due to a limitation
         // of the terminal (...usually)
@@ -128,5 +129,114 @@ pub mod input {
         Scroll_Down {x: u16, y: u16, modifier: Modifier}
     }
 
-    
-}
+    /// If coming from a UTF-8 character subtract 33!
+    /// This is simply a backend for me because I'm
+    /// not smart enough to figure out how Rust and
+    /// Termion do it! 
+    pub fn parse_char (character: u8) -> char {
+        match character {
+            0 => '!',
+            1 => '"',
+            2 => '#',
+            3 => '$',
+            4 => '%',
+            5 => '&',
+            6 => '\'',
+            7 => '(',
+            8 => ')',
+            9 => '*',
+            10 => '+',
+            11 => ',',
+            12 => '-',
+            13 => '.',
+            14 => '/',
+            // Numbers
+            15 => '0',
+            16 => '1',
+            17 => '2',
+            18 => '3',
+            19 => '4',
+            20 => '5',
+            21 => '6',
+            22 => '7',
+            23 => '8',
+            24 => '9',
+            // Special Characters
+            25 => ':',
+            26 => ';',
+            27 => '<',
+            28 => '=',
+            29 => '>',
+            30 => '?',
+            31 => '@',
+            // Capitalized Characters
+            32 => 'A',
+            33 => 'B',
+            34 => 'C',
+            35 => 'D',
+            36 => 'E',
+            37 => 'F',
+            38 => 'G',
+            39 => 'H',
+            40 => 'I',
+            41 => 'J',
+            42 => 'K',
+            43 => 'L',
+            44 => 'M',
+            45 => 'N',
+            46 => 'O',
+            47 => 'P',
+            48 => 'Q',
+            49 => 'R',
+            50 => 'S',
+            51 => 'T',
+            52 => 'U',
+            53 => 'V',
+            54 => 'W',
+            55 => 'X',
+            56 => 'Y',
+            57 => 'Z',
+            // Special Characters
+            58 => '[',
+            59 => '\\',
+            60 => ']',
+            61 => '^',
+            62 => '_',
+            63 => '`',
+            // Lowercase Characters
+            64 => 'a',
+            65 => 'b',
+            66 => 'c',
+            67 => 'd',
+            68 => 'e',
+            69 => 'f',
+            70 => 'g',
+            71 => 'h',
+            72 => 'i',
+            73 => 'j',
+            74 => 'k',
+            75 => 'l',
+            76 => 'm',
+            77 => 'n',
+            78 => 'o',
+            79 => 'p',
+            80 => 'q',
+            81 => 'r',
+            82 => 's',
+            83 => 't',
+            84 => 'u',
+            85 => 'v',
+            86 => 'w',
+            87 => 'x',
+            88 => 'y',
+            89 => 'z',
+            // Special Characters
+            90 => '{',
+            91 => '|',
+            92 => '}',
+            93 => '~',
+            _ => panic!("{} Is not a number that's covered!", character)
+
+        }
+    }
+} 
