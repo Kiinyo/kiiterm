8dd206b47cfc8291cc2d0269cb8e30eeedfbf919
Kiinyo <iiyoheki@gmail.com>
2021-02-10 20:06:24 -0700

    testing and debugging parse_input

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,18 +1,35 @@
 extern crate kiiterm;
-use std::io::{Write};
+use std::io::*;
+
+use kiiterm::screen::input::*;
 fn main() {
     let mut screen = kiiterm::screen::init(100, 5);
 
     loop {
-        let mut buffer = Vec::new();
-        use std::io::*;
+
+        let mut buffer: Vec<u8> = Vec::new();
+
         screen.inputs.read_to_end(&mut buffer).unwrap();
+
+        let inputs: Vec<Key> = parse_input(&buffer);
+
         writeln!(screen.context,
-        "{}{}Buffer: {:?}",
+        "{}{}Buffer: {:?} Interpreted Inputs: {:?}",
         termion::clear::All,
         termion::cursor::Goto(1, 1),
-        buffer)
+        buffer,
+        inputs)
        .unwrap();
+
+        if inputs.len() > 0 {
+            match inputs[0] {
+                Key::Ctrl_Char(c) => {
+                    if c == 'C' {break;}
+                },
+                _ => {}
+            }
+       }
+
 
         screen.context.flush().unwrap();
         std::thread::sleep(std::time::Duration::from_millis(2000));
