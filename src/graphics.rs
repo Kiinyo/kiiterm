38726f877bc403678b922d33097a76979dbe5778
Kiinyo <iiyoheki@gmail.com>
2021-02-14 16:06:38 -0700

    added parse_color

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,3 +1,60 @@
-pub enum Style {}
+/// Styles
+pub enum Style {
+    Bold,
+    Underscore,
+    Blink
+}
+fn parse_style (style: Style) {
 
-pub enum Color {}
+}
+/// Colors
+pub enum Color {
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+
+    RGB(u8, u8, u8)
+}
+pub enum Depth {
+    Fg,
+    Bg
+}
+pub fn parse_color (color: Color, depth: Depth) -> String {
+    let mut value: u8;
+    let code: String;
+    let mut color_tuple: (u8, u8, u8) = (0, 0, 0);
+
+    match color {
+        Color::Black => value = 30,
+        Color::Red => value = 31,
+        Color::Green => value = 32,
+        Color::Yellow => value = 33,
+        Color::Blue => value = 34,
+        Color::Magenta => value = 35,
+        Color::Cyan => value = 36,
+        Color::White => value = 37,
+
+        Color::RGB(r, g, b) => {
+            value = 38;
+            color_tuple = (r, g, b);
+        }
+    }
+
+    match depth {
+        Depth::Fg => value += 0,
+        Depth::Bg => value += 10
+    }
+
+    if value != 38 && value != 48 {
+        code = format!("\u{001B}[{:?}m", value);
+    } else {
+        code = format!("\u{001B}[{:?};{};{};{}m", value, color_tuple.0, color_tuple.1, color_tuple.2)
+    }
+
+    code
+}
