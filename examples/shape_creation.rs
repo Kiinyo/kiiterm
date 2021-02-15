28263656803c0e959767042b887592fad3586473
Kiinyo <iiyoheki@gmail.com>
2021-02-15 00:45:13 -0700

    added create_rectangle

--- examples/shape_creation.rs
+++ examples/shape_creation.rs
@@ -1,5 +1,14 @@
 fn main() {
-    let grid = kiiterm::grid::create(5, 3, 0);
-    // Debug print grid
-    println!("{:?}", grid);
+    // Check grid
+    let grid = kiiterm::grid::create_grid(5, 3, 0);
+    println!("Grid: {:?}", grid);
+    // Check empty rect
+    let empty_rect = kiiterm::grid::create_rectangle(3, 3, 5, 0);
+    println!("Empty Rectangle: {:?}", empty_rect);
+    // Check filled rect
+    let filled_rect = kiiterm::grid::create_rectangle(3, 5, 3, 3);
+    println!("Filled Rectangle: {:?}", filled_rect);
+    // Chece edge case tiny rect
+    let tiny_rect = kiiterm::grid::create_rectangle(1, 1, 3, 3);
+    println!("Tiny Rect: {:?}", tiny_rect);
 }
--- src/grid.rs
+++ src/grid.rs
@@ -3,7 +3,7 @@ pub struct Grid {
     pub height: u16,
     pub tiles: Vec<Vec<u8>>
 }
-
+/// Implimenting debugging because I know I'm going to need it.
 impl std::fmt::Debug for Grid {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let mut y:usize = 0;
@@ -17,30 +17,50 @@ impl std::fmt::Debug for Grid {
     }
 
 }
-
-pub fn create (width: u16, height: u16, fill: u8) -> Grid {
+/// Create a grid with with width and height and fill it.
+pub fn create_grid (width: u16, height: u16, fill: u8) -> Grid {
     // Loop to fill in everything
-    let mut x: u16 = 0;
-    let mut y: u16 = 0;
     let mut tiles: Vec<Vec<u8>> = Vec::new();
-    loop {
-        if y >= height {break;}
+    for _y in 0..height {
         let mut row: Vec<u8> = Vec::new();
-
-        loop {
+        for x in 0..width{
             if x >= width {break;}
             row.push(fill); 
-            x += 1;
         }
         tiles.push(row);
-
-        x = 0;
-        y += 1;
     }
-
+    // And return it
     Grid {
         width,
         height,
         tiles
+    }
+}
+/// Create a rectangle grid
+pub fn create_rectangle (width: u16, height: u16, border: u8, fill: u8) -> Grid {
+    let mut tiles: Vec<Vec<u8>> = Vec::new();
+    let w_usize:usize = width as usize;
+    if border == fill {
+        for _y in 0..height {
+            let row = vec![fill; w_usize];
+            tiles.push(row);
+        }
+    } else {
+        for y in 0..height {
+            if y == 0 || y == height - 1 {
+                let row: Vec<u8> = vec![border; w_usize];
+                tiles.push(row);
+            } else {
+                let mut row: Vec<u8> = vec![fill; w_usize];
+                row[0] = border;
+                row[w_usize - 1] = border;
+                tiles.push(row);
+            }
+        }
+    }
+    Grid {
+        width,
+        height,
+        tiles,
     }
 }
