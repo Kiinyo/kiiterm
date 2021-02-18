9db7535654460d2833f3d0edeabde4dabd6232ae
Kiinyo <iiyoheki@gmail.com>
2021-02-17 23:50:14 -0700

    fleshed out elements struct

---
+++ src/ui/element.rs
@@ -0,0 +1,30 @@
+pub struct element {
+    // Identifiers
+    name: String,
+    desc: String,
+    elem_id: usize,
+    // Things the element can do
+    elem_type: Element_Type,
+    // Physical properties
+    position: crate::ui::Position,
+    size: crate::ui::Size,
+    appearance: crate::ui::Appearance
+}
+
+pub enum Element_Type {
+    Default,
+    Interactive {
+        press: Element_Action,
+        release: Element_Action,
+
+        click: Element_Action,
+        abandon: Element_Action,
+
+        drag: Element_Action,
+
+        scroll: Element_Action,
+    }
+}
+pub enum Element_Action {
+    None
+}
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,2 +1,82 @@
+/// Where the widget is in coordinate space.
+pub struct Position {
+    // Where the ui piece actually is
+    current_x: usize,
+    current_y: usize,
+    // Where it wants to be
+    target_x: usize,
+    target_y: usize,
+    // How it'll get there
+    Animation: Animation<Reposition>
+}
+/// The possible ways to animate a repositing widget.
+pub enum Reposition {
+    None
+}
+/// The amount of space the widget takes up.
+pub struct Size {
+    current_width: usize,
+    current_height: usize,
+
+    target_width: usize,
+    target_height: usize,
+
+    Animation: Animation<Resize>
+}
+/// The possible ways to animate resizing a widget.
+pub enum Resize {
+    None
+}
+pub struct Appearance {
+    // The color of the body of a text box
+    primary_color: crate::graphics::Color,
+    // The color of the bar along the top
+    secondary_color: crate::graphics::Color,
+    // The color of any text in the box
+    tertiary_color: crate::graphics::Color,
+
+    text: Text,
+    
+    // The shape of the widget itself
+    shape: crate::grid::Shape,
+    // Animation of the appearance
+    animation: crate::ui::Animation<Shader>,
+    // The actual grid of (pixels), saves time
+    // when calling up draw functions.
+    buffer: crate::grid::Grid
+}
+pub struct Text {
+    string: Vec<crate::graphics::Glyph>,
+    // Alignment of the text
+    align_x: Align_X,
+    align_y: Align_Y,
+    // How far from the edge of the widget text can be displayed
+    padding_x: usize,
+    padding_y: usize,
+    // The actual grid of (pixels), saves time
+    // when calling up draw functions.
+    buffer: crate::grid::Grid
+}
+pub enum Align_X {
+    Left,
+    Center,
+    Right
+}
+pub enum Align_Y {
+    Up,
+    Center,
+    Down
+}
+pub enum Shader {
+    None
+}
+/// Animation container
+pub struct Animation<Animation_Type> {
+    // Details about what frame the animation is in
+    current_frame: usize,
+    target_frame: usize,
+    // Info for actually parsing the animation.
+    animation_type: Animation_Type,
+}
 pub mod element;
 pub mod container;
