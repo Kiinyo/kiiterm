995a2df379d30c3d7ee0fbba69bc9a5fe4211031
Kiinyo <iiyoheki@gmail.com>
2021-02-19 23:58:30 -0700

    making fields public

--- src/ui/container.rs
+++ src/ui/container.rs
@@ -1,19 +1,20 @@
 pub struct Container {
     // Identifiers
-    name: String,
-    desc: String,
-    cont_id: usize,
+    pub name: String,
+    pub desc: String,
+    pub cont_id: usize,
     // Things the element can do
-    cont_type: Container_Type,
+    pub cont_type: Container_Type,
     // Physical properties
-    position: crate::ui::Position,
-    size: crate::ui::Size,
-    appearance: crate::ui::Appearance,
+    pub position: crate::ui::Position,
+    pub size: crate::ui::Size,
+    pub appearance: crate::ui::Appearance,
     // Keeping track of elements
-    elem_id: usize,
-    elements: Vec<crate::ui::element::Element>,
-    element_buffer: crate::grid::Grid
+    pub elem_id: usize,
+    pub elements: Vec<crate::ui::element::Element>,
+    pub element_buffer: crate::grid::Grid
 }
 pub enum Container_Type {
     Default
 }
+
