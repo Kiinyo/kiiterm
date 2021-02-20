e2f862c8cc607916bfee29640e5c6fb709789ee9
Kiinyo <iiyoheki@gmail.com>
2021-02-20 04:15:55 -0700

    learning how to impl clone

--- examples/ui_creator.rs
+++ examples/ui_creator.rs
@@ -1,3 +1,19 @@
+use kiiterm::graphics::Glyph;
+use kiiterm::graphics::Color;
+use kiiterm::graphics::Style;
+
 fn main() {
-    println!("Can I print chars?: {}", 'a');
+    let glyph1 = Glyph {
+        symbol: "A".to_string(),
+        fg_color: Color::Black,
+        bg_color: Color::Default,
+        styles: vec![Style::Strike_Through]
+    };
+
+    let mut glyph2 = glyph1.clone();
+
+    glyph2.symbol = "B".to_string();
+
+    println!("Does glyph1 work? {}", glyph1.symbol);
+    println!("Does glyph1 work? {}", glyph2.symbol);
 }
