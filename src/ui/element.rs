529d874a15bc26550cd17417c06fadbaa3b0cc0d
Kiinyo <iiyoheki@gmail.com>
2021-02-19 23:58:40 -0700

    added create

--- src/ui/element.rs
+++ src/ui/element.rs
@@ -1,3 +1,4 @@
+use crate::ui::*;
 pub struct Element {
     // Identifiers
     name: String,
@@ -6,11 +7,11 @@ pub struct Element {
     // Things the element can do
     elem_type: Element_Type,
     // Physical properties
-    position: crate::ui::Position,
-    size: crate::ui::Size,
-    appearance: crate::ui::Appearance,
+    position: Position,
+    size: Size,
+    appearance: Appearance,
     // Template for later use/reference
-    template: Element_Type
+    template: Element_Template
 }
 
 pub enum Element_Type {
@@ -39,19 +40,127 @@ pub struct Element_Template {
     desc: String,
     elem_type: Element_Type,
     // Percentage of Container (<=1)
-    align_x: crate::ui::Align_X,
+    align_x: Align_X,
     offset_x: f32,
-    align_y: crate::ui::Align_Y,
+    align_y: Align_Y,
     offset_y: f32,
     // Percentage of Container (<=1)
     scale_width: f32,
     scale_height: f32,
     // Shape
+    body: crate::graphics::Glyph,
     shape: crate::grid::Shape,
     // Text handling
     text: String,
-    text_align_x: crate::ui::Align_X,
-    text_align_y: crate::ui::Align_Y,
+    text_align_x: Align_X,
+    text_align_y: Align_Y,
     padding_x: usize,
     padding_y: usize
+}
+
+pub fn create (container: container::Container, template: Element_Template) -> Element {
+
+    let size = Size {
+        // Current
+        current_width: (container.size.current_width as f32 * template.scale_width) as usize,
+        current_height: (container.size.current_height as f32 * template.scale_height) as usize,
+        // Target
+        target_width: (container.size.target_width as f32 * template.scale_width) as usize,
+        target_height: (container.size.target_height as f32 * template.scale_height) as usize,
+        animation: Animation {
+            current_frame: 0,
+            target_frame: 0,
+            animation_type: Resize::None,
+            
+        }
+    };
+    
+    let position = Position {
+        current_x: match template.align_x {
+            Align_X::Left => {
+                0
+            }
+            Align_X::Center => {
+                container.size.current_width / 2 - size.current_width / 2
+            }
+            Align_X::Right => {
+                container.size.current_width - size.current_width
+            }
+        },
+        current_y: match template.align_y {
+            Align_Y::Up => {
+                0
+            }
+            Align_Y::Center => {
+                container.size.current_height / 2 - size.current_height / 2
+            }
+            Align_Y::Down => {
+                container.size.current_height - size.current_height
+            }
+        },
+        target_x: match template.align_x {
+            Align_X::Left => {
+                0
+            }
+            Align_X::Center => {
+                container.size.target_width / 2 - size.target_width / 2
+            }
+            Align_X::Right => {
+                container.size.target_width - size.target_width
+            }
+        },
+        target_y: match template.align_y {
+            Align_Y::Up => {
+                0
+            }
+            Align_Y::Center => {
+                container.size.target_height / 2 - size.target_height / 2
+            }
+            Align_Y::Down => {
+                container.size.target_height - size.target_height
+            }
+        },
+        animation: Animation {
+            current_frame: 0,
+            target_frame: 0,
+            animation_type: Reposition::None,
+            
+        }
+    };
+
+    let appearance = Appearance {
+        body: template.body,
+        text: Text {
+            string: crate::graphics::parse_string_to_glyphs(template.text),
+            align_x: template.text_align_x,
+            align_y: template.text_align_y,
+            padding_x: template.padding_x,
+            padding_y: template.padding_y,
+            buffer: crate::grid::create_grid(1,1, 0)
+        },
+        shape: template.shape,
+        animation: Animation {
+            current_frame: 0,
+            target_frame: 0,
+            animation_type: Shader::None,
+            
+        },
+        buffer: crate::grid::create_grid(1,1, 0)
+
+    };
+
+    let element: Element = Element {
+        name: template.name,
+        desc: template.desc,
+        elem_id: container.elem_id,
+        elem_type: template.elem_type,
+        position,
+        size,
+        appearance,
+        template
+    };
+
+    container.elem_id += 1;
+
+    return element;
 }
